b0eb04c56fba353c0d63549b74650e8ba469f9f3
Dhagz99 <kimjanrey123@gmail.com>
2026-03-03 13:56:41 +0800

    deploy march 3

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,8 +6,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import http from "http";
 import { Server } from "socket.io";
-import routes from "../src/routes/urls";
-import "./cron/yearlySave.cron";
+import routes from './routes/urls';
 
 dotenv.config();
 
